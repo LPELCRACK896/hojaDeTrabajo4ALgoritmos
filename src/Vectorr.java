488dc8df693c714c8635f5001527a7d262a90f18
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2021-03-03 12:35:04 -0600

    Implementada clase Vectorr.java

--- src/Vectorr.java
+++ src/Vectorr.java
@@ -1,26 +1,41 @@
+import java.util.Vector;
+
 public class Vectorr <E> implements Stack<E>{
+    Vector<E> stack;
+
+    public Vectorr(){
+        stack= new Vector<E>(0);
+
+    }
     @Override
     public void push(E item) {
-
+        stack.add(item);
     }
 
     @Override
     public E pop() {
-        return null;
+        if(stack.isEmpty())return null;
+        else{
+            E temporal =stack.elementAt(stack.size()-1);
+            stack.remove(stack.size()-1);
+            return temporal;
+        }
     }
 
     @Override
     public E peek() {
-        return null;
+        if(!empty())return stack.elementAt(stack.size()-1);
+        else return null;
     }
 
     @Override
     public boolean empty() {
-        return false;
+        return stack.isEmpty();
     }
 
     @Override
     public int size() {
-        return 0;
+        return stack.size();
     }
+
 }
