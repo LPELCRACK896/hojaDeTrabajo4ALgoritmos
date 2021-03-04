77811be136e41bcfb13fa06d7b4356e346362a8d
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2021-03-04 07:43:00 -0600

    Añadi link git a main, prueba JUnit y actualizacion archivo PDF

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+/**
+ * @author Axel Lopez
+ * @author Luis Gonzalez
+ * GIT: https://github.com/LPELCRACK896/hojaDeTrabajo4ALgoritmos.git
+ */
 public class main {
 
     public static void main(String[] args){
